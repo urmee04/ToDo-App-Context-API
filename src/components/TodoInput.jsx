b5b8da6d49d9f1a0bfabd20fb0109376246f92a7
urmee04 <urmee04sust@gmail.com>
2025-08-12 15:05:31 -0400

    add TodoInput component with form handling and context integration

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { TodoContext } from "./contexts/TodoContext";
+
+const TodoInput = () => {
+  //destructure addTodo function from TodoContext using useContext
+  const { addTodo } = useContext(TodoContext);
+
+  //local state to store the input value for the new todo
+  const [text, setText] = React.useState("");
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault(); //Prevent default form submit behavior,page reload
+    addTodo(text); //Call context's addTodo function to add a new todo
+    setText(""); //Clear the input field after submission
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {" "}
+      <input
+        type="text"
+        value={text} // bind input value to local state
+        onChange={(e) => setText(e.target.value)} //update state when user types
+        placeholder="Add new todo"
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default TodoInput;
