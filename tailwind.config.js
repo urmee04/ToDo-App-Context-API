2c5f5e2fbaf02e5abd55471e8d4da10fdf6abeef
urmee04 <urmee04sust@gmail.com>
2025-08-12 23:48:45 -0400

    add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   darkMode: "class",
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+module.exports = {
+  darkMode: "class", // enable class-based dark mode
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
